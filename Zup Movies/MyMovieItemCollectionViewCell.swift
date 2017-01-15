106cb9c177140aaecdad4a068ea1bbadbd5e21ce
Adolfho Athyla <adolfo.athyla@gmail.com>
2017-01-15 17:18:29 -0300

    Replace TableViewController to CollectionViewController in MyMovies

---
+++ Zup Movies/MyMovieItemCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyMovieItemCollectionViewCell.swift
+//  Zup Movies
+//
+//  Created by Adolfho Athyla on 15/01/17.
+//  Copyright © 2017 a7hyla. All rights reserved.
+//
+
+import UIKit
+
+class MyMovieItemCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var moviePoster: UIImageView!
+    @IBOutlet var movieTitle: UILabel!
+}
---
+++ Zup Movies/MyMoviesCollectionViewController.swift
@@ -0,0 +1,114 @@
+//
+//  MyMoviesCollectionViewController.swift
+//  Zup Movies
+//
+//  Created by Adolfho Athyla on 15/01/17.
+//  Copyright © 2017 a7hyla. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "MY_MOVIE_IDENTIFIER"
+
+class MyMoviesCollectionViewController: UICollectionViewController {
+
+    var movies = [Movie]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.isNavigationBarHidden = false
+        
+        if let movies = Movie.getAllMovies() {
+            self.movies = movies
+        }
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+//        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? MyMovieItemCollectionViewCell
+    
+        cell?.moviePoster.image = movies[indexPath.row].poster
+        cell?.movieTitle.text = movies[indexPath.row].title
+    
+        return cell!
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+
+}
+
+
+
+extension MyMoviesCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.size.width
+        return CGSize(width: (width/2)-17, height: (width/2)-17)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+    }
+}
